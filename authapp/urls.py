a4111cd132bcaeb6ecee18ca7e55f901f5de7676
multiverseweb <multiverse.letter@gmail.com>
2025-04-10 10:52:25 +0530

    test

--- authapp/urls.py
+++ authapp/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from .views import register_view, login_view, verify_otp_view, home_view
 
 urlpatterns = [
+    path('', register_view, name='register'),
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('verify/', verify_otp_view, name='verify_otp'),
     path('home/', home_view, name='home'),
+    
 ]
